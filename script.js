cde4eebd0875cd4b9090f7208049beedc0eb5a71
David Robison <david@robisonweb.dev>
2021-02-18 10:31:19 -0700

    Setup Book Constructor, Connected addBookToLibrary to HTML, Push new book to myLibrary, and set addBookToLibrary prototype to new onject that has copy of Book.prototype

--- script.js
+++ script.js
@@ -2,11 +2,23 @@
 let myLibrary = [];
 
 // Book Constructor
-function Book() {
-
+function Book(author, title, pages, read) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = read;
 }
 
 // Function that adds book to myLibrary Array
 function addBookToLibrary() {
-    
+    const author = document.querySelector('#authorForm').value;
+    const title = document.querySelector('#titleForm').value;
+    const pages = document.querySelector('#pagesForm').value;
+    const read = document.querySelector(`input[name='read']:checked`).value;
+
+    const newBook = new Book(author, title, pages, read);
+
+    myLibrary.push(newBook);
 }
+
+addBookToLibrary.prototype = Object.create(Book.prototype);
