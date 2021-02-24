3a9487eaf9a1ebbc9821a506efdaf526f0699a29
David Robison <david@robisonweb.dev>
2021-02-24 11:59:10 -0700

    Created two functions, one function adds eventlisteners to status button and the other changes the status on click

--- script.js
+++ script.js
@@ -47,46 +47,6 @@ function addBookToLibrary() {
 
 addBookToLibrary.prototype = Object.create(Book.prototype);
 
-function displayBookList() {
-    deleteBookList();
-
-    myLibrary.forEach((book) => {
-        const bookListTBody = document.querySelector('tbody');
-        const bookListTR = document.createElement('tr');
-        const titleTD = document.createElement('td');
-        const authorTD = document.createElement('td');
-        const pagesTD = document.createElement('td');
-        const statusTD = document.createElement('td');
-        const editTD = document.createElement('td');
-        const editBtn = document.createElement('i');
-
-        bookListTR;
-        titleTD.innerText = book.title;
-        authorTD.innerText = book.author;
-        pagesTD.innerText = book.pages;
-        pagesTD.classList.add('centerAlign');
-        statusTD.innerText = book.status;
-        statusTD.classList.add('centerAlign');
-        editTD.classList.add('centerAlign');
-        editBtn.setAttribute('class', 'las la-edit');
-        editBtn.setAttribute('data-key', book.dataKey);
-
-        editTD.appendChild(editBtn);
-        bookListTR.appendChild(titleTD);
-        bookListTR.appendChild(authorTD);
-        bookListTR.appendChild(pagesTD);
-        bookListTR.appendChild(statusTD);
-        bookListTR.appendChild(editTD);
-        bookListTBody.appendChild(bookListTR);
-    })
-}
-
-function deleteBookList() {
-    const bookListTBody = document.querySelector('tbody');
-
-    bookListTBody.innerHTML = '';
-}
-
 function bookInputToggle() {
     const addToLibrary = document.querySelector('#bookInput');
     const openBookInputBtn = document.querySelector('#openBookInputBtn');
@@ -98,6 +58,45 @@ function bookInputToggle() {
         addToLibrary.style.display = 'none';
         openBookInputBtn.classList.remove('removeDisplay');
     }
+}
+
+function displayBookList() {
+    deleteBookList();
+
+    myLibrary.forEach((book) => {
+        const bookListTBody = document.querySelector('tbody');
+        const bookListTR = document.createElement('tr');
+        const titleTD = document.createElement('td');
+        const authorTD = document.createElement('td');
+        const pagesTD = document.createElement('td');
+        const statusTD = document.createElement('td');
+        const statusBtn = document.createElement('button');
+        const deleteTD = document.createElement('td');
+        const deleteBtn = document.createElement('i');
+
+        bookListTR;
+        titleTD.innerText = book.title;
+        authorTD.innerText = book.author;
+        pagesTD.innerText = book.pages;
+        pagesTD.classList.add('centerAlign');
+        statusTD.classList.add('centerAlign');
+        statusBtn.setAttribute('type', 'button');
+        statusBtn.setAttribute('class', 'statusBtns');
+        statusBtn.setAttribute('data-key', book.dataKey)
+        statusBtn.innerText = book.status;
+        deleteTD.classList.add('centerAlign');
+        deleteBtn.setAttribute('class', 'las la-trash');
+        deleteBtn.setAttribute('data-key', book.dataKey);
+
+        statusTD.appendChild(statusBtn);
+        deleteTD.appendChild(deleteBtn);
+        bookListTR.appendChild(titleTD);
+        bookListTR.appendChild(authorTD);
+        bookListTR.appendChild(pagesTD);
+        bookListTR.appendChild(statusTD);
+        bookListTR.appendChild(deleteTD);
+        bookListTBody.appendChild(bookListTR);
+    })
 }
 
 function deleteBookInputValues() {
@@ -112,6 +111,16 @@ function deleteBookInputValues() {
     status.selectedIndex = 0;
 }
 
+function deleteBookList() {
+    const bookListTBody = document.querySelector('tbody');
+
+    bookListTBody.innerHTML = '';
+}
+
+function deleteObjectInArray(array, start, end) {
+    array.splice(start, end);
+}
+
 function findObjectInArray(key, array) {
     for (let i = 0; i < array.length; i++) {
         if (array[i].dataKey == key) {
@@ -120,18 +129,38 @@ function findObjectInArray(key, array) {
     }
 }
 
-function deleteObjectInArray(array, start, end) {
-    array.splice(start, end);
+function statusBtnsEventListener() {
+    const statusBtns = document.querySelectorAll('.statusBtns')
+
+    statusBtns.forEach((btn) => {
+        btn.addEventListener('click', (e) => {
+            statusChange(btn, e.target.dataset.key);
+        })
+    })
+}
+
+function statusChange(btn, key) {
+    if (btn.textContent == 'Read') {
+        myLibrary[findObjectInArray(key, myLibrary)].status = 'Reading';    
+    } else if (btn.textContent == 'Reading') {
+        myLibrary[findObjectInArray(key, myLibrary)].status = 'Not Read';
+    } else if (btn.textContent == 'Not Read') {
+        myLibrary[findObjectInArray(key, myLibrary)].status = 'Read';
+    }
+
+    displayBookList();
+    statusBtnsEventListener();
 }
 
 displayBookList();
+statusBtnsEventListener();
 
 
 // Eventlisteners 
 const openBookInputBtn = document.querySelector('#openBookInputBtn');
 const submitBtn = document.querySelector('#submit');
 const cancelBtn = document.querySelector('#cancel');
-const editBtns = document.querySelectorAll('.las');
+
 
 openBookInputBtn.addEventListener('click', () => {
     bookInputToggle();
@@ -149,11 +178,36 @@ cancelBtn.addEventListener('click', () => {
     deleteBookInputValues();
 })
 
-editBtns.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-        deleteObjectInArray(myLibrary, findObjectInArray(e.target.dataset.key, myLibrary), 1);  // For Test, remove later
 
-        console.log(myLibrary);
-    })
-})
 
+
+
+
+// OLD CODE
+
+// function editBtnsEventListeners() {
+//     const editBtns = document.querySelectorAll('.las');
+
+//     editBtns.forEach((btn) => {
+//         btn.addEventListener('click', (e) => {
+//             // deleteObjectInArray(myLibrary, findObjectInArray(e.target.dataset.key, myLibrary), 1);  // For Test, remove later
+//             bookInputToggle();
+//             editBook(myLibrary, findObjectInArray(e.target.dataset.key, myLibrary));
+//         })
+//     })
+// }
+
+// function editBook(array, position) {
+//     const header = document.querySelector('#formHeader');
+//     const author = document.querySelector('#authorForm');
+//     const title = document.querySelector('#titleForm');
+//     const pages = document.querySelector('#pagesForm');
+//     const status = document.querySelector('#status');
+
+//     header.innerText = 'Edit Book';
+
+//     author.value = array[position].author;
+//     title.value = array[position].title;
+//     pages.value = array[position].pages;
+//     status.value = array[position].status;
+// }
