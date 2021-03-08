10ec93509febb32348a4bc4d45781ea09164dba5
David Robison <david@robisonweb.dev>
2021-03-08 12:06:56 -0700

    Moved myLibrary into books module

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
-// Array to store book objects
-let myLibrary = [
-    {
-        author: 'David Patrick',
-        title: 'Wind Hunter',
-        pages: '523',
-        status: 'Read',
-        dataKey: 'DavidRobison_523',
-    },
-    {
-        author: 'Patrick Rothfuss',
-        title: 'The Name of the Wind',
-        pages: '662',
-        status: 'Reading',
-        dataKey: 'PatrickRothfuss_662',
-    },
-    {
-        author: 'Robert Jordan',
-        title: 'The Great Hunt',
-        pages: '706',
-        status: 'Not Read',
-        dataKey: 'RobertJordan_706',
-    },
-];
-
 const books = (function() {
-    
+    // Array to store book objects
+    let myLibrary = [
+        {
+            author: 'David Patrick',
+            title: 'Wind Hunter',
+            pages: '523',
+            status: 'Read',
+            dataKey: 'DavidRobison_523',
+        },
+        {
+            author: 'Patrick Rothfuss',
+            title: 'The Name of the Wind',
+            pages: '662',
+            status: 'Reading',
+            dataKey: 'PatrickRothfuss_662',
+        },
+        {
+            author: 'Robert Jordan',
+            title: 'The Great Hunt',
+            pages: '706',
+            status: 'Not Read',
+            dataKey: 'RobertJordan_706',
+        },
+    ];
 })();
 
 // Book Constructor
