9525a671b3434249cb3c85450ac8bd73e1a96ab7
David Robison <david@robisonweb.dev>
2021-04-09 17:57:30 -0600

    Changed class name, and append bookForm() onto div

--- src/addBookButton.js
+++ src/addBookButton.js
@@ -1,15 +1,18 @@
+import bookForm from "./bookForm";
+
 function addBookButton() {
     const div = document.createElement('div');
     const button = document.createElement('button');
 
     div.setAttribute('id', 'addBookDiv');
     button.setAttribute('id', 'addBookButton');
-    button.setAttribute('class', 'libraryFormButtons');
+    button.setAttribute('class', 'bookFormButtons');
     button.setAttribute('type', 'button');
 
     button.innerText = 'Add Book';
 
     div.appendChild(button);
+    div.appendChild(bookForm());
 
     return div;
 }
