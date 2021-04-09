4521909f480c0dd6b2e02ceae787d02c6d148123
David Robison <david@robisonweb.dev>
2021-04-09 15:31:04 -0600

    Created function that make a button inside a div, and returns div

---
+++ src/addBookButton.js
@@ -0,0 +1,16 @@
+function addBookButton() {
+    const div = document.createElement('div');
+    const button = document.createElement('button');
+
+    div.setAttribute('id', 'addBookDiv');
+    button.setAttribute('id', 'addBookButton');
+    button.setAttribute('type', 'button');
+
+    button.innerText = 'Add Book';
+
+    div.appendChild(button);
+
+    return div;
+}
+
+export default addBookButton;
