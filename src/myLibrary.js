b91a753dd570f2b0f61563fd027e076235ba1c27
David Robison <david@robisonweb.dev>
2021-04-10 16:21:27 -0600

    Added array name myLibrary with temp objects

---
+++ src/myLibrary.js
@@ -0,0 +1,25 @@
+let myLibrary = [
+    {
+        author: 'David Robison',
+        title: 'Wind Hunter',
+        pages: '523',
+        status: 'Read',
+        dataKey: 'DavidRobison_523',
+    },
+    {
+        author: 'Patrick Rothfuss',
+        title: 'The Name of the Wind',
+        pages: '662',
+        status: 'Reading',
+        dataKey: 'PatrickRothfuss_662',
+    },
+    {
+        author: 'Robert Jordan',
+        title: 'The Great Hunt',
+        pages: '706',
+        status: 'Not Read',
+        dataKey: 'RobertJordan_706',
+    },
+];
+
+export default myLibrary;
