b25cb3922d9bf4ee8c714f0c4378bc4ced09b2de
David Robison <david@robisonweb.dev>
2021-04-10 16:12:47 -0600

    Called cancelFormButtonEventListener()

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import container from "./container";
+import cancelFormButtonEventListener from "./eventListeners/cancelFormButtonEventListener";
 import footer from "./footer";
 
 const body = document.querySelector('body');
 
 body.appendChild(container());
 body.appendChild(footer('https://github.com/robisonwebdev', 'David Robison'));
+
+cancelFormButtonEventListener();
