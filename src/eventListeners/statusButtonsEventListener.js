ca48b2a8867a9e41eb3e269bb41f94d98080fa7d
David Robison <david@robisonweb.dev>
2021-04-11 10:59:13 -0600

    Called refreshDisplayBooks()

--- src/eventListeners/statusButtonsEventListener.js
+++ src/eventListeners/statusButtonsEventListener.js
@@ -1,8 +1,10 @@
+import refreshDisplayBooks from '../refreshDisplayBooks';
 import statusChange from '../statusChange';
 
 function statusButtonsEventListener(button) {
     button.addEventListener('click', (e) => {
         statusChange(button, e.target.dataset.key);
+        refreshDisplayBooks();
     })
 }
 
