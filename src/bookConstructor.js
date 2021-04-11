edce7e704080b750ca93f90ecb8c3397c202304a
David Robison <david@robisonweb.dev>
2021-04-11 15:40:35 -0600

    Copied code from old script.js and put into own file and exported

---
+++ src/bookConstructor.js
@@ -0,0 +1,11 @@
+class book {
+    constructor(author, title, pages, status, dataKey) {
+        this.author = author;
+        this.title = title;
+        this.pages = pages;
+        this.status = status;
+        this.dataKey = dataKey;
+    }
+}
+
+export default book;
