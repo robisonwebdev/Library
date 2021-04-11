149432bf389b0484b377849d5f9d554e699a35e5
David Robison <david@robisonweb.dev>
2021-04-11 10:45:04 -0600

    Change how eventListner is added

--- src/eventListeners/statusButtonsEventListener.js
+++ src/eventListeners/statusButtonsEventListener.js
@@ -1,12 +1,8 @@
 import statusChange from '../statusChange';
 
-function statusButtonsEventListener() {
-    const statusBtns = document.querySelectorAll('.statusBtns')
-
-    statusBtns.forEach((btn) => {
-        btn.addEventListener('click', (e) => {
-            statusChange(btn, e.target.dataset.key);
-        })
+function statusButtonsEventListener(button) {
+    button.addEventListener('click', (e) => {
+        statusChange(button, e.target.dataset.key);
     })
 }
 
