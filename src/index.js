c023735d5ac74e7de5a6f31e293fed4ee4f1f8f0
David Robison <david@robisonweb.dev>
2021-04-12 10:01:32 -0600

    Called formSubmitEventListener

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import container from "./container";
 import cancelFormButtonEventListener from "./eventListeners/cancelFormButtonEventListener";
+import formSubmitEventListener from "./eventListeners/formSubmitEventListener";
 import footer from "./footer";
 
 const body = document.querySelector('body');
@@ -7,4 +8,5 @@ const body = document.querySelector('body');
 body.appendChild(container());
 body.appendChild(footer('https://github.com/robisonwebdev', 'David Robison'));
 
+formSubmitEventListener();
 cancelFormButtonEventListener();
