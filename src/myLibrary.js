fb7580c6a8e6520b5d70366508fb1d5ea6be4293
David Robison <david@robisonweb.dev>
2021-04-12 10:02:03 -0600

    Removed trailing ,

--- src/myLibrary.js
+++ src/myLibrary.js
@@ -4,22 +4,22 @@ let myLibrary = [
         title: 'Wind Hunter',
         pages: '523',
         status: 'Read',
-        dataKey: 'DavidRobison_523',
+        dataKey: 'DavidRobison_523'
     },
     {
         author: 'Patrick Rothfuss',
         title: 'The Name of the Wind',
         pages: '662',
         status: 'Reading',
-        dataKey: 'PatrickRothfuss_662',
+        dataKey: 'PatrickRothfuss_662'
     },
     {
         author: 'Robert Jordan',
         title: 'The Great Hunt',
         pages: '706',
         status: 'Not Read',
-        dataKey: 'RobertJordan_706',
-    },
+        dataKey: 'RobertJordan_706'
+    }
 ];
 
 export default myLibrary;
