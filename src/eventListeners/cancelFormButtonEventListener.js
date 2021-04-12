179ea2a31a3e67bbb6e3c01ac001d00e9763c240
David Robison <david@robisonweb.dev>
2021-04-12 08:52:00 -0600

    Called clearForm()

--- src/eventListeners/cancelFormButtonEventListener.js
+++ src/eventListeners/cancelFormButtonEventListener.js
@@ -1,9 +1,11 @@
 import bookFormToggle from "../bookFormToggle";
+import clearForm from "../clearForm";
 
 function cancelFormButtonEventListener() {
     const cancelFormButton = document.querySelector('#cancel');
 
     cancelFormButton.addEventListener('click', () => {
+        clearForm();
         bookFormToggle();
     });
 }
